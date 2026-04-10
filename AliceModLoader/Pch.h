4bd66221ee02d5df7eaf7a7a934bf16e75cc7cc5
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2026-04-10 22:16:41 +0500

    Other changes that I needed to make it compile on my PC

--- AliceModLoader/Pch.h
+++ AliceModLoader/Pch.h
@@ -11,7 +11,7 @@
 #include <iostream>
 
 #define BASE_ADDRESS 0x400000
-#include "Helpers.h"
-#include "INIReader.h"
+#include "Dependencies/Helpers.h"
+#include "Dependencies/INIReader.h"
 #include "D3D9Hook.h"
 #include "FileService.h"
